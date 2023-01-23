c2525e76414894e6c6c4ece8dfb40df7896a4531
heebum99 <heebum9955@naver.com>
2023-01-23 21:40:09 +0900

    Initial_Settings

---
+++ src/test/java/heeboo/springpractice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package heeboo.springpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
