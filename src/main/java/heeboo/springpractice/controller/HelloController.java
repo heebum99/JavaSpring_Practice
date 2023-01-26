bbbf370977b39ff96f37c50d6a1d8e127e490fa9
heebum99 <heebum9955@naver.com>
2023-01-26 15:31:29 +0900

    빌드&실행

--- src/main/java/heeboo/springpractice/controller/HelloController.java
+++ src/main/java/heeboo/springpractice/controller/HelloController.java
@@ -4,6 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//빌드, 실행하기
+/*
+1.cmd를 통해 현재 디렉토리에서 gradle.bat build => 빌드
+2.cd build
+3.cd libs
+4.libs 디렉토리 안에 만들어져있는 jar파일 실행
+=> java -jar 파일명 =>실행
+
+안되는 경우 현재 디렉토리에서 clean build 먼저 실행후 1번부터 진행
+*/
 @Controller
 public class HelloController {
 
