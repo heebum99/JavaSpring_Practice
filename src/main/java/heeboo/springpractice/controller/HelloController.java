fd9e596a1431c686c68676e2387a937fde0c3f51
heebum99 <heebum9955@naver.com>
2023-01-26 16:42:58 +0900

    MVC, 템플릿 엔진

--- src/main/java/heeboo/springpractice/controller/HelloController.java
+++ src/main/java/heeboo/springpractice/controller/HelloController.java
@@ -3,6 +3,7 @@ package heeboo.springpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //빌드, 실행하기
 /*
@@ -22,5 +23,15 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
         //return hello =>ViewResolver가 resources/templates/hello.html을 찾아 렌더링
+    }
+
+    //MVC: Model, View, Controller => 역할분담
+    //View => 화면을 그리는것에만 집중
+    //Controller => 비지니스로직, 내부 로직을 처리하는데만 집중
+    @GetMapping("hello-mvc")
+    //외부에서 Parameter를 받기 위해 @RequestParam 사용
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
     }
 }
